d466eff2d3d6f6dfc7ccc9bfc86d6e54d415a257
ignaciobecher <ignacio.becher@gmail.com>
2026-03-13 11:43:43 -0300

    Refactor code structure for improved readability and maintainability

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
-  logger.log(`BugMate is running on port ${port}`);
+  logger.log(`****BugMate is running on port ${port}***`);
 }
 bootstrap();
